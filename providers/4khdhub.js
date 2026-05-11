15aa83ee796a0bb53809530fc6f912f4c9f7c40e
Antonio Ante <47629459+Xyr0nX@users.noreply.github.com>
2026-05-12 07:26:39 +0800

    Update 4khdhub.js

--- providers/4khdhub.js
+++ providers/4khdhub.js
@@ -22,7 +22,7 @@ var PROVIDER_NAME = "4khdhub";
 var DOMAINS_URL = "https://raw.githubusercontent.com/Xyr0nX/NGEX/refs/heads/main/manifest.json";
 var DEFAULT_MAIN_URL = "https://4khdhub.dad";
 var TMDB_API_KEY = "439c478a771f35c05022f9feabcca01c";
-var DEBUG = true;
+var DEBUG = false;
 
 var FALLBACK_DOMAINS = [DEFAULT_MAIN_URL];
 // Manual fallback untuk film/serial yang tidak muncul di pencarian
